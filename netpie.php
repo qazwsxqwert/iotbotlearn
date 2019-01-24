1adbc673491ce76f40670552c5d8f9f641f79db3
QwertyP <32947577+qazwsxqwert@users.noreply.github.com>
2019-01-24 15:26:16 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "linebot/"; //enter your appid
+		$KEY = "I62zSWdwhl1ZeLp"; //enter your key
+		$SECRET = "TwKySXz3Qbzk1JZGXoBjx5Fev"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
